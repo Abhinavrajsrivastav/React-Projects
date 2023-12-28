1819af819c8908caadd69afb03ab25033e31f69d
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2023-12-29 00:49:27 +0530

    29-12-2023-001

--- Musics/hinlish/src/playlist.js
+++ Musics/hinlish/src/playlist.js
@@ -24,7 +24,7 @@ function chillHop() {
     {
       name: "Legends",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+        "images/legends.jpg",
       artist: "Aiguille",
       audio: "/musics/legends.mp3",
       color: ["#EF8EA9", "#ab417f"],
