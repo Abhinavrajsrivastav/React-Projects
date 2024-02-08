28468381f318d18a14972b9069408a83776f7560
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2024-02-09 02:04:08 +0530

    09-02-2024-001

--- Musics/hinlish/src/playlist.js
+++ Musics/hinlish/src/playlist.js
@@ -4,20 +4,20 @@ function chillHop() {
     {
       name: "Loki Epic",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg",
+        "images/one-direction.jpg",
       artist: "Makzo",
       audio: "/musics/one-direction.mp3",
-      color: ["#47609D", "#E88774"],
+      color: ["#99BC85", "#12372A"],
       id: uuidv4(),
       active: true,
     },
     {
       name: "Glorious Purpose",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+        "images/Love.jpg",
       artist: "Aso, Middle School, Aviino",
       audio: "/musics/photo_graph.mp3",
-      color: ["#205950", "#2ab3bf"],
+      color: ["#E1F0DA", "#643843"],
       id: uuidv4(),
       active: false,
     },
@@ -27,14 +27,14 @@ function chillHop() {
         "images/legends.jpg",
       artist: "Aiguille",
       audio: "/musics/legends.mp3",
-      color: ["#EF8EA9", "#ab417f"],
+      color: ["#C9D7DD", "#7D0A0A"],
       id: uuidv4(),
       active: false,
     },
     {
       name: "Hall of fame",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+        "images/Champian.jpg",
       artist: "Swørn",
       audio: "/musics/The_Script_-_Hall_Of_Fame_Lyrics_(getmp3.pro).mp3",
       color: ["#CD607D", "#c94043"],
@@ -44,47 +44,47 @@ function chillHop() {
     {
       name: "Coldplay",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/10/737bb830d34592344eb4a2a1d2c006cdbfc811d9-1024x1024.jpg",
+        "images/ColdPlay.jpg",
       artist: "Swørn",
       audio: "musics/The_Chainsmokers_Coldplay_-_Somet_(getmp3.pro).mp3",
-      color: ["#335561", "#3A393E"],
+      color: ["#335561", "#3887BE"],
       id: uuidv4(),
       active: false,
     },
     {
       name: "Shineing in the shadow",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/5bff1a6f1bd0e2168d29b4c841b811598135e457-1024x1024.jpg",
+        "images/TeraHoneLagaHu.jpg",
       artist: "Misha, Jussi Halme",
       audio: "musics/Tera_Hone_Laga_Hoon_Lyrical_-_Ajab_(getmp3.pro).mp3",
-      color: ["#2A416D", "#E98087"],
+      color: ["#2A416D", "#FF9800"],
       id: uuidv4(),
       active: false,
     },
     {
       name: "Subhanallah",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+        "images/SubhanAllah.jpg",
       artist: "Swørn",
       audio: "musics/Subhanallah_-_Yeh_Jawaani_Hai_Deewa_(getmp3.pro).mp3",
-      color: ["#BD3D76", "#551853"],
+      color: ["#FDBF60", "#191919"],
       id: uuidv4(),
       active: false,
     },
     {
-      name: "For my name",
+      name: "Mere naam tu",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/06/49f6e32ca521fbad46a1b281e3893cf6254bf11d-1024x1024.jpg",
+        "images/MereNaamTu.jpg",
       artist: "Strehlow, Aylior",
       audio: "musics/Mere_Naam_Tu_From_Zero_(getmp3.pro).mp3",
-      color: ["#F2C5AB", "#BE5853"],
+      color: ["#FEFBF6", "#BE5853"],
       id: uuidv4(),
       active: false,
     },
     {
       name: "Aditi",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/8404541e3b694d16fd79433b142ed910f36764dd-1024x1024.jpg",
+        "images/Aditi.jpg",
       artist: "Cloudchord, G Mills",
       audio: "musics/Lyrical_Kabhi_Kabhi_Aditi_Zindagi_(getmp3.pro).mp3",
       color: ["#FF194A", "#41B3B3"],
@@ -94,93 +94,43 @@ function chillHop() {
     {
       name: "Glorious Purpose",
       cover:
-        "images/boy.jpg",
+        "images/Beautiful.jpg",
       artist: "Philanthrope",
       audio: "musics/beautiful_people.mp3",
-      color: ["#E36414","#4CB9E7"],
+      color: ["#4CB9E7","#E36414"],
       id: uuidv4(),
       active: false,
     },
     {
-      name: "Nightfall",
+      name: "Ilaahi",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+            "images/Illahi.jpg",
       artist: "Illahi",
       audio: "musics/Illahi_-_Yeh_Jawaani_Hai_Deewani__(getmp3.pro).mp3",
-      color: ["#EF8EA9", "#ab417f"],
+      color: ["#FF9130", "#B15EFF"],
       id: uuidv4(),
       active: false,
     },
     {
       name: "Let me love you",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+        "images/LetMeLoveYou.jpg",
       artist: "Swørn",
       audio: "musics/DJ_Snake_-_Let_Me_Love_You_ft_Just_(getmp3.pro).mp3",
-      color: ["#CD607D", "#c94043"],
+      color: ["#00A9FF", "#3A4D39"],
       id: uuidv4(),
       active: false,
     },
     {
       name: "Challa",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/7a84488fd87082302cb69c05262f2f3f87e93018-1024x1024.jpg",
+        "images/Challah.jpg",
       artist: "Hanz",
       audio: "musics/Challa_Full_Song_Jab_Tak_Hai_Ja_(getmp3.pro).mp3",
       color: ["#90B4AA", "#F2AE87"],
       id: uuidv4(),
       active: false,
     },
-    {
-      name: "Eastway",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/c572841e8431cebc120dffed4f92119f723dd954-1024x1024.jpg",
-      artist: "Dontcry, Nokiaa",
-      audio: "musics/make_you_mine.mp3",
-      color: ["#B442A1", "#F54F7A"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Wake up",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/2c3bd458bfb0713c89f991d1ce469523e95e3b53-1024x1024.jpg",
-      artist: "Evil Needle",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=8285",
-      color: ["#A35CA0", "#EE8D66"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Under the City Stars",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
-      artist: "Aso, Middle School, Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
-      color: ["#205950", "#2ab3bf"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Velocities",
-      cover: "https://i.scdn.co/image/ab67616d0000b2734fb6a52430e65dbc6c593faf",
-      artist: "Sleepy Fish",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=3524",
-      color: ["#74c7b4", "#1d6d92"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Deeper",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/10/23fdd99adc3e16abcb67b004ea3e748ebf433a49-1024x1024.jpg",
-      artist: "Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10460",
-      color: ["#C668B2", "#4D92D7"],
-      id: uuidv4(),
-      active: false,
-    },
-
     //ADD MORE HERE
   ];
 }
